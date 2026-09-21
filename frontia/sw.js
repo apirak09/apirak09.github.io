3c703ddd0ef484cfa70b1736474810412bd1ce5c
apirak09 <108285958+apirak09@users.noreply.github.com>
2026-09-22 00:58:36 +0700

    Upgrade PWA cache to v5

--- frontia/sw.js
+++ frontia/sw.js
@@ -1,5 +1,5 @@
-const CACHE="cinematic-play-v4";
-const CORE=["./","./index.html","./styles.css?v=4","./app.js?v=4","./manifest.webmanifest"];
+const CACHE="cinematic-play-v5";
+const CORE=["./","./index.html","./styles.css?v=5","./app.js?v=5","./manifest.webmanifest?v=5"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
